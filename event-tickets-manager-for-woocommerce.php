42b2a2c57d13e24631a9bb9188cedb543be8e702
PrinceKumarYadav <me.prince729@gmail.com>
2021-11-17 12:24:04 +0530

    version_update

--- event-tickets-manager-for-woocommerce.php
+++ event-tickets-manager-for-woocommerce.php
@@ -15,15 +15,15 @@
  * Plugin Name:       Event Tickets Manager for WooCommerce
  * Plugin URI:        https://wordpress.org/plugins/event-tickets-manager-for-woocommerce/
  * Description:       Event Tickets Manager for WooCommerce allows you to manage, sell and assign tickets easily.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            MakeWebBetter
  * Author URI:          https://makewebbetter.com/?utm_source=MWB-event-org&utm_medium=MWB-org-backend &utm_campaign=MWB-event-site
  * Text Domain:       event-tickets-manager-for-woocommerce
  * Domain Path:       /languages
  * Requires at least: 4.6
- * Tested up to:      5.8
+ * Tested up to:      5.8.2
  * WC requires at least: 4.0
- * WC tested up to:   5.6.0
+ * WC tested up to:   5.9.0
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -43,7 +43,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	 */
 	function define_event_tickets_manager_for_woocommerce_constants() {
 
-		event_tickets_manager_for_woocommerce_constants( 'EVENT_TICKETS_MANAGER_FOR_WOOCOMMERCE_VERSION', '1.0.2' );
+		event_tickets_manager_for_woocommerce_constants( 'EVENT_TICKETS_MANAGER_FOR_WOOCOMMERCE_VERSION', '1.0.3' );
 		event_tickets_manager_for_woocommerce_constants( 'EVENT_TICKETS_MANAGER_FOR_WOOCOMMERCE_DIR_PATH', plugin_dir_path( __FILE__ ) );
 		event_tickets_manager_for_woocommerce_constants( 'EVENT_TICKETS_MANAGER_FOR_WOOCOMMERCE_DIR_URL', plugin_dir_url( __FILE__ ) );
 		event_tickets_manager_for_woocommerce_constants( 'EVENT_TICKETS_MANAGER_FOR_WOOCOMMERCE_SERVER_URL', 'https://makewebbetter.com' );
