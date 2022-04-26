e87943971a79bbe5810709882609e934461feec0
rishabhmwb <rishabhsingh@makewebbetter.com>
2022-04-26 08:22:57 +0530

    notice removed

--- event-tickets-manager-for-woocommerce.php
+++ event-tickets-manager-for-woocommerce.php
@@ -332,36 +332,5 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 
 }
 
-add_action( 'after_plugin_row_' . plugin_basename( __FILE__ ), 'event_upgrade_notice', 0, 3 );
 
-/**
- * Displays notice to WPSWings.
- *
- * @param string $plugin_file Path to the plugin file relative to the plugins directory.
- * @param array  $plugin_data An array of plugin data.
- * @param string $status Status filter currently applied to the plugin list.
- */
-function event_upgrade_notice( $plugin_file, $plugin_data, $status ) {
-
-	?>
-<tr class="plugin-update-tr active notice-warning notice-alt">
-	<td colspan="4" class="plugin-update colspanchange">
-		<div class="notice notice-success inline update-message notice-alt">
-			<div class='wps-notice-title wps-notice-section'>
-				<p><strong>IMPORTANT NOTICE:</strong></p>
-			</div>
-			<div class='wps-notice-content wps-notice-section'>
-				<p>From this update <strong>Version 1.0.4</strong> onwards, the plugin and its support will be handled by <strong>WP Swings</strong>.</p><p><strong>WP Swings</strong> is just our improvised and rebranded version with all quality solutions and help being the same, so no worries at your end.
-				Please connect with us for all setup, support, and update related queries without hesitation.</p>
-			</div>
-		</div>
-	</td>
-</tr>
-<style>
-	.wps-notice-section > p:before {
-		content: none;
-	}
-</style>
-	<?php
-}
 
