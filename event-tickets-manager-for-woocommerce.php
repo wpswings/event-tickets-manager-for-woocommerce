2b1e932a9b5eb6a4dc2f3646402ccf725c0617de
rishabhmwb <rishabhsingh@makewebbetter.com>
2022-08-23 12:05:34 +0530

    wc wp updated

--- event-tickets-manager-for-woocommerce.php
+++ event-tickets-manager-for-woocommerce.php
@@ -23,7 +23,7 @@
  * Requires at least:    4.6
  * Tested up to:         6.0.1
  * WC requires at least: 4.0
- * WC tested up to:      6.7.0
+ * WC tested up to:      6.8.1
  * License:              GNU General Public License v3.0
  * License URI:          http://www.gnu.org/licenses/gpl-3.0.html
  */
