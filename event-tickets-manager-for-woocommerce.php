8d43a568deb88e57c19c250da73d13264c7521b4
prince-cedcoss <princekumaryadav@makewebbetter.com>
2023-04-20 11:40:45 +0530

    langugae_update

--- event-tickets-manager-for-woocommerce.php
+++ event-tickets-manager-for-woocommerce.php
@@ -21,9 +21,9 @@
  * Text Domain:          event-tickets-manager-for-woocommerce
  * Domain Path:          /languages
  * Requires at least:    4.6
- * Tested up to:         6.1.1
+ * Tested up to:         6.2.0
  * WC requires at least: 4.0
- * WC tested up to:      7.4.1
+ * WC tested up to:      7.6.0
  * License:              GNU General Public License v3.0
  * License URI:          http://www.gnu.org/licenses/gpl-3.0.html
  */
